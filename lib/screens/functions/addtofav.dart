7707f1a7c8845eb8ad7a2a01daed228ec2762dd0
SirajMM <sirajmuhammed718@gmail.com>
2023-04-18 10:59:59 +0530

    added more functionalities

--- lib/screens/functions/addtofav.dart
+++ lib/screens/functions/addtofav.dart
@@ -6,29 +6,6 @@ import 'package:blaze_player/model/songmodel.dart';
 import 'package:blaze_player/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-// addToFavorite(int index) async {
-//   List<Songs> dbSongs = box.values.toList();
-//   List<FavSongs> favoritesongs = [];
-//   favoritesongs = favsongsdb.values.toList();
-//   bool isempty = favoritesongs
-//       .where((element) => element.songname == dbSongs[index].songname)
-//       .isEmpty;
-//   if (isempty) {
-//     favsongsdb.add(FavSongs(
-//         songname: dbsongs[index].songname,
-//         artist: dbSongs[index].artist,
-//         duration: dbSongs[index].duration,
-//         songurl: dbSongs[index].songurl,
-//         id: dbSongs[index].id));
-//   } else {
-//     favoritesongs
-//         .where((element) => element.songname == dbSongs[index].songname)
-//         .isEmpty;
-//     int currentindex =
-//         favoritesongs.indexWhere((element) => element.id == dbSongs[index].id);
-//     await favsongsdb.deleteAt(currentindex);
-//   }
-// }
 addToFavorite(int id) async {
   List<Songs> dbSongs = box.values.toList();
   List<FavSongs> favoritesongs = favsongsdb.values.toList();
@@ -48,23 +25,6 @@ addToFavorite(int id) async {
   }
 }
 
-// bool checkFavourite(int index, BuildContext) {
-//   List<FavSongs> favouritesongs = [];
-//   List<Songs> dbsongs = box.values.toList();
-
-//   FavSongs value = FavSongs(
-//       songname: dbsongs[index].songname,
-//       artist: dbsongs[index].artist,
-//       duration: dbsongs[index].duration,
-//       songurl: dbsongs[index].songurl,
-//       id: dbsongs[index].id);
-
-//   favouritesongs = favsongsdb.values.toList();
-//   bool ispresent = favouritesongs
-//       .where((element) => element.songname == value.songname)
-//       .isEmpty;
-//   return ispresent ? true : false;
-// }
 bool checkFavourite(int? songId, BuildContext) {
   List<FavSongs> favouritesongs = [];
   List<Songs> dbsongs = box.values.toList();
@@ -84,14 +44,6 @@ bool checkFavourite(int? songId, BuildContext) {
   return isPresent;
 }
 
-// removeFav(int index) async {
-//   final favbox = FavBox.getInstance();
-//   List<FavSongs> favsongs = favbox.values.toList();
-//   List<Songs> dbsongs = box.values.toList();
-//   int currentindex =
-//       favsongs.indexWhere((element) => element.id == dbsongs[index].id);
-//   await favsongsdb.deleteAt(currentindex);
-// }
 Future<void> removeFav(int songId) async {
   final favbox = FavBox.getInstance();
   List<FavSongs> favsongs = favbox.values.toList();
